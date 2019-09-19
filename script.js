ab0f5e101d8459cb3f092eee19e7816b3ec2a1d5
jnmcshan <54905760+jnmcshan@users.noreply.github.com>
2019-09-19 16:06:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+Start;
+<script>
+document.getElementById("Calculator").innerHTML = "My Fun with Conditions in JavaScript";
+</script>
+var callIt = document.body.querySelector("Calculator");
+var num1 = ("number1");
+  if (NaN); {
+  greeting = "End"; //  block of code to be executed if the condition is true
+} 
+{
+   
+var num2 = ("number2");
+  if (NaN); {
+  greeting = "End"; //  block of code to be executed if the condition is true 
+ 
+} {
+var sum1 = ((num1 "(+| -| x| /)" num2)); 
+    switch="+,-,*, /" "Operator";
+      case "add=>+";
+      case "subtract=>-";
+      case "multiply=>*;
+      case "divide=>/";
+      
+}
+  {                  
+console.log("num1 (+|-|x|/) "num2");{
+           
+
+callIt.innerHTML = "Calculator";       
+     if (+ == "Add"); 
+      CalculatorEle.innerHTML = "Addition";
+     if (- == "Subtract"); 
+      CalculatorEle.innerHTML = "Subtraction";
+     if (x == "Multiply"); 
+      CalculatorEle.innerHTML = "Multiply";
+     if (/ == "Subtract"); 
+      CalculatorEle.innerHTML="Divide";
+} 
+ {      
+switch (operator); 
+  case "+":
+    callIt.innerHTML = "Add";
+    break;
+    
+  case "-":
+    callIt.innerHTML = "Subtract";
+    break;
+
+  case "x":
+    callIt.innerHTML = "Multiply";
+    break;
+    
+  case "/":
+    callIt.innerHTML = "Divide";
+    break;    
+} 
+End;
